7dc0d5d40d32df128a873ccf2cb1a3bfced0dd10
Ashish Rathaur <121791024+iamashishrathaur@users.noreply.github.com>
2024-01-14 16:27:52 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,12 @@ AnBRoutes(app)
 const port = process.env.PORT || 3000;
 
 database();
-
+app.use(express.static(__dirname, './dist/index.html', { type: 'module' }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, './dist/index.html'));
 })
